be24114d6931de774938f961b25271a62791e141
mohamad <m.aleik@darsoft.net>
2021-12-28 14:09:20 +0200

    Updates

---
+++ precache-manifest.b2b3bd5450e11bb3522fbf671539a0ec.js
@@ -0,0 +1,478 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6240398ffc0feb6a3dc28f205ce5c4",
+    "url": "/%7Bex-pay%7D/index.html"
+  },
+  {
+    "revision": "91531b689b29af11dea5",
+    "url": "/%7Bex-pay%7D/static/css/2.7295f7f4.chunk.css"
+  },
+  {
+    "revision": "a9fd36741470efcb2b5c",
+    "url": "/%7Bex-pay%7D/static/css/main.57d5e5cd.chunk.css"
+  },
+  {
+    "revision": "91531b689b29af11dea5",
+    "url": "/%7Bex-pay%7D/static/js/2.349f84da.chunk.js"
+  },
+  {
+    "revision": "04d500169f2fcd9e569c56aa191f0782",
+    "url": "/%7Bex-pay%7D/static/js/2.349f84da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fd36741470efcb2b5c",
+    "url": "/%7Bex-pay%7D/static/js/main.8ab654a3.chunk.js"
+  },
+  {
+    "revision": "9088ed194db1b05f5905",
+    "url": "/%7Bex-pay%7D/static/js/runtime-main.a914dc67.js"
+  },
+  {
+    "revision": "8491142e5f7a2422821cfb0fd37dbdcb",
+    "url": "/%7Bex-pay%7D/static/media/0111.8491142e.png"
+  },
+  {
+    "revision": "8a3fc4d6f94415a9086612a9e1792bba",
+    "url": "/%7Bex-pay%7D/static/media/0222.8a3fc4d6.png"
+  },
+  {
+    "revision": "5228f5ca38815ab27f6f1764c7c22fb6",
+    "url": "/%7Bex-pay%7D/static/media/03.5228f5ca.png"
+  },
+  {
+    "revision": "ccd738c3fc97989cc4ca378f6cb920d0",
+    "url": "/%7Bex-pay%7D/static/media/04.ccd738c3.png"
+  },
+  {
+    "revision": "c3a524694cb3ce764c477a469a575350",
+    "url": "/%7Bex-pay%7D/static/media/05.c3a52469.png"
+  },
+  {
+    "revision": "4c55977b8ea920f81fa0507564f7cfb6",
+    "url": "/%7Bex-pay%7D/static/media/06.4c55977b.png"
+  },
+  {
+    "revision": "7de9c3245a978281563254bb2cf4cf3e",
+    "url": "/%7Bex-pay%7D/static/media/ElegantIcons.7de9c324.svg"
+  },
+  {
+    "revision": "d72ad3f702b9f23540e8ed78b4b65749",
+    "url": "/%7Bex-pay%7D/static/media/ElegantIcons.d72ad3f7.eot"
+  },
+  {
+    "revision": "f9d179f59b0878ffcd32a5b3c8ae9c62",
+    "url": "/%7Bex-pay%7D/static/media/ElegantIcons.f9d179f5.ttf"
+  },
+  {
+    "revision": "fdd9e757bf61675343dcf55100422b84",
+    "url": "/%7Bex-pay%7D/static/media/ElegantIcons.fdd9e757.woff"
+  },
+  {
+    "revision": "0bd6650144b8fcc5dfcda3d9663a12c4",
+    "url": "/%7Bex-pay%7D/static/media/Img-01.0bd66501.jpg"
+  },
+  {
+    "revision": "6b70b87d803e98b4b9bc7d3780828c32",
+    "url": "/%7Bex-pay%7D/static/media/Img-02.6b70b87d.jpg"
+  },
+  {
+    "revision": "5eee01908949ddd6aafd52d1a8b49f82",
+    "url": "/%7Bex-pay%7D/static/media/Img-03.5eee0190.jpg"
+  },
+  {
+    "revision": "c20bf45f5b5ff92d8c2a890addeef6c0",
+    "url": "/%7Bex-pay%7D/static/media/Img-05.c20bf45f.jpg"
+  },
+  {
+    "revision": "ed7cdcb27f436d74741e560a3ae50e1d",
+    "url": "/%7Bex-pay%7D/static/media/Img-07.ed7cdcb2.jpg"
+  },
+  {
+    "revision": "596814caa4fbaecbf5014bcfe8e363fb",
+    "url": "/%7Bex-pay%7D/static/media/Simple-Line-Icons.596814ca.ttf"
+  },
+  {
+    "revision": "c9c9b5ebc6395eeb83072bf18e3818aa",
+    "url": "/%7Bex-pay%7D/static/media/Simple-Line-Icons.c9c9b5eb.svg"
+  },
+  {
+    "revision": "f19a7f6c7a0b54b748277c40d7cf8882",
+    "url": "/%7Bex-pay%7D/static/media/Simple-Line-Icons.f19a7f6c.eot"
+  },
+  {
+    "revision": "ff94ad94c3a9d04bd2f80cb3c87dcccb",
+    "url": "/%7Bex-pay%7D/static/media/Simple-Line-Icons.ff94ad94.woff"
+  },
+  {
+    "revision": "5c702f58fd644df5e72c8c0846ad9177",
+    "url": "/%7Bex-pay%7D/static/media/app_screen.5c702f58.png"
+  },
+  {
+    "revision": "07df75e71906063b10f9df62b4fe0797",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_1.07df75e7.jpg"
+  },
+  {
+    "revision": "09248d2fa1f391a37e577bff0d3a1fab",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_2.09248d2f.jpg"
+  },
+  {
+    "revision": "4d0cc3925587c847d17441e56f427da5",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_3.4d0cc392.jpg"
+  },
+  {
+    "revision": "d94e7cf0a004f152f058ead09c65279f",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_4.d94e7cf0.jpg"
+  },
+  {
+    "revision": "dc59bc48af645592355018a49073f606",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_5.dc59bc48.jpg"
+  },
+  {
+    "revision": "a09386c5b38deecd27e1d7e96cd636b3",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_6.a09386c5.jpg"
+  },
+  {
+    "revision": "f74ea2743f7751706e089c4b4a16ad0e",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_7.f74ea274.jpg"
+  },
+  {
+    "revision": "79c71cd8952e667f44b2e3cc7412f02d",
+    "url": "/%7Bex-pay%7D/static/media/blog_grid_8.79c71cd8.jpg"
+  },
+  {
+    "revision": "b2c7920282bb9900eaf3688add8d8655",
+    "url": "/%7Bex-pay%7D/static/media/blog_list1.b2c79202.jpg"
+  },
+  {
+    "revision": "20167d95929ea60af5601e3683df7f0d",
+    "url": "/%7Bex-pay%7D/static/media/blog_list2.20167d95.jpg"
+  },
+  {
+    "revision": "abb2ebb615098a07ef828efc4574af70",
+    "url": "/%7Bex-pay%7D/static/media/blog_list3.abb2ebb6.jpg"
+  },
+  {
+    "revision": "68afa1170ffc7852a2bf870b8bdf3c33",
+    "url": "/%7Bex-pay%7D/static/media/blog_list4.68afa117.jpg"
+  },
+  {
+    "revision": "ad855a819e4a97d395c13ba01e1fdef8",
+    "url": "/%7Bex-pay%7D/static/media/blog_single.ad855a81.png"
+  },
+  {
+    "revision": "8be588ace181db5cf9964243314091a5",
+    "url": "/%7Bex-pay%7D/static/media/case_01.8be588ac.jpg"
+  },
+  {
+    "revision": "e403f022cea7ad1509b2ef85af30d7a2",
+    "url": "/%7Bex-pay%7D/static/media/case_02.e403f022.jpg"
+  },
+  {
+    "revision": "e1cc4e13e911a1bef7b99a62468450b0",
+    "url": "/%7Bex-pay%7D/static/media/case_03.e1cc4e13.jpg"
+  },
+  {
+    "revision": "d43f53b2119f1ab4d0749776cc99bf91",
+    "url": "/%7Bex-pay%7D/static/media/case_04.d43f53b2.jpg"
+  },
+  {
+    "revision": "5d03ae50de742c31132257e4d91df2e6",
+    "url": "/%7Bex-pay%7D/static/media/case_05.5d03ae50.jpg"
+  },
+  {
+    "revision": "17c849fdb89c8b9c6b9a19541e5e4b09",
+    "url": "/%7Bex-pay%7D/static/media/case_07.17c849fd.jpg"
+  },
+  {
+    "revision": "00955b24a4be4fb059ce1a49f9c7b410",
+    "url": "/%7Bex-pay%7D/static/media/case_08.00955b24.jpg"
+  },
+  {
+    "revision": "0f803e611524aae254783e6f1d064ffa",
+    "url": "/%7Bex-pay%7D/static/media/case_09.0f803e61.jpg"
+  },
+  {
+    "revision": "98e9ec2bf36e52bd58dd68e909ab12c8",
+    "url": "/%7Bex-pay%7D/static/media/case_10.98e9ec2b.jpg"
+  },
+  {
+    "revision": "85eed07af0fa9ff65ebec6d40456fa72",
+    "url": "/%7Bex-pay%7D/static/media/case_11.85eed07a.jpg"
+  },
+  {
+    "revision": "653dcc267dafae63d2ed736aff14644f",
+    "url": "/%7Bex-pay%7D/static/media/case_12.653dcc26.jpg"
+  },
+  {
+    "revision": "c0a1b5c7b0b73bb6f6ea3c2715233e4c",
+    "url": "/%7Bex-pay%7D/static/media/case_details_01.c0a1b5c7.jpg"
+  },
+  {
+    "revision": "fad1c41a768b0cedee208a6ecd5f9c52",
+    "url": "/%7Bex-pay%7D/static/media/case_details_02.fad1c41a.jpg"
+  },
+  {
+    "revision": "5c06ceaacaa114cc62a053778cbdc839",
+    "url": "/%7Bex-pay%7D/static/media/case_details_03.5c06ceaa.jpg"
+  },
+  {
+    "revision": "aefaf54902c9cfe48cfc957c7e94d8e2",
+    "url": "/%7Bex-pay%7D/static/media/coming_soon_bg.aefaf549.png"
+  },
+  {
+    "revision": "5a657f6f4a2fd16272175ad6a448893c",
+    "url": "/%7Bex-pay%7D/static/media/company.5a657f6f.jpg"
+  },
+  {
+    "revision": "3f8615e1c5020510928f425db414d987",
+    "url": "/%7Bex-pay%7D/static/media/contact.3f8615e1.png"
+  },
+  {
+    "revision": "0f4332f2c1400ac974e001bacf2b7907",
+    "url": "/%7Bex-pay%7D/static/media/error.0f4332f2.png"
+  },
+  {
+    "revision": "d710f00e93b029cd0d7f93e7dcbc160c",
+    "url": "/%7Bex-pay%7D/static/media/error_bg.d710f00e.png"
+  },
+  {
+    "revision": "4568b5b5034960068b1cd0f69dab2039",
+    "url": "/%7Bex-pay%7D/static/media/fa-brands-400.4568b5b5.svg"
+  },
+  {
+    "revision": "cbd387d93e253048800dc9ee22b85c24",
+    "url": "/%7Bex-pay%7D/static/media/fa-brands-400.cbd387d9.eot"
+  },
+  {
+    "revision": "d30c44aaad8efa7626428c8294f7e880",
+    "url": "/%7Bex-pay%7D/static/media/fa-brands-400.d30c44aa.woff"
+  },
+  {
+    "revision": "d6ac6c968cff1abcbf5d548828b9f6c6",
+    "url": "/%7Bex-pay%7D/static/media/fa-brands-400.d6ac6c96.ttf"
+  },
+  {
+    "revision": "eac60e8a656781e13d2a674b4d9051c0",
+    "url": "/%7Bex-pay%7D/static/media/fa-brands-400.eac60e8a.woff2"
+  },
+  {
+    "revision": "26d8edc83280c58a277393d469e7b26f",
+    "url": "/%7Bex-pay%7D/static/media/fa-regular-400.26d8edc8.svg"
+  },
+  {
+    "revision": "4b218302f9057d02864d4909661831e9",
+    "url": "/%7Bex-pay%7D/static/media/fa-regular-400.4b218302.woff2"
+  },
+  {
+    "revision": "8c1f078070537f81237a2e39eaae2555",
+    "url": "/%7Bex-pay%7D/static/media/fa-regular-400.8c1f0780.eot"
+  },
+  {
+    "revision": "e32cb360659f3788d9f4b5750c2c5a36",
+    "url": "/%7Bex-pay%7D/static/media/fa-regular-400.e32cb360.woff"
+  },
+  {
+    "revision": "f1994ecd58b56afa035ae3da39213357",
+    "url": "/%7Bex-pay%7D/static/media/fa-regular-400.f1994ecd.ttf"
+  },
+  {
+    "revision": "2e302fa4c6eeb1bc06149067bae3e7b4",
+    "url": "/%7Bex-pay%7D/static/media/fa-solid-900.2e302fa4.eot"
+  },
+  {
+    "revision": "5dc01cfcd5336f696cb85da7ce53fa9b",
+    "url": "/%7Bex-pay%7D/static/media/fa-solid-900.5dc01cfc.woff2"
+  },
+  {
+    "revision": "80c404ff42e52d9e7589e83fe21307b4",
+    "url": "/%7Bex-pay%7D/static/media/fa-solid-900.80c404ff.ttf"
+  },
+  {
+    "revision": "a8eedaadb16b569a48a061d4aafa2d2e",
+    "url": "/%7Bex-pay%7D/static/media/fa-solid-900.a8eedaad.woff"
+  },
+  {
+    "revision": "b7c60297fd4238ed7bf0df6f8842c3df",
+    "url": "/%7Bex-pay%7D/static/media/fa-solid-900.b7c60297.svg"
+  },
+  {
+    "revision": "b60ab1ebf82a0a2a794c7e88f8d0d237",
+    "url": "/%7Bex-pay%7D/static/media/featured_img.b60ab1eb.png"
+  },
+  {
+    "revision": "00ba8ada124bb73c6788a19a7990f7e1",
+    "url": "/%7Bex-pay%7D/static/media/featured_img_two.00ba8ada.png"
+  },
+  {
+    "revision": "17d53ea5d3429b2aebf306264500d5d6",
+    "url": "/%7Bex-pay%7D/static/media/features_01.17d53ea5.png"
+  },
+  {
+    "revision": "dae615f0978928a2dbabcd61fe1218f4",
+    "url": "/%7Bex-pay%7D/static/media/feedback_shap.dae615f0.png"
+  },
+  {
+    "revision": "6f2f9f37f375293ac6c7e91a2eb81e75",
+    "url": "/%7Bex-pay%7D/static/media/iPhoneX.6f2f9f37.png"
+  },
+  {
+    "revision": "ab491a468f7c6819b17bc40012c660ce",
+    "url": "/%7Bex-pay%7D/static/media/laptop.ab491a46.png"
+  },
+  {
+    "revision": "82e3798ffc9853b26104cccac49c3f79",
+    "url": "/%7Bex-pay%7D/static/media/laptop_two.82e3798f.png"
+  },
+  {
+    "revision": "a2056a5da624f457afd42d3774dada73",
+    "url": "/%7Bex-pay%7D/static/media/line_01.a2056a5d.png"
+  },
+  {
+    "revision": "1d4895e20b49a2d8a681acd2e73c3c4e",
+    "url": "/%7Bex-pay%7D/static/media/login_img.1d4895e2.png"
+  },
+  {
+    "revision": "9616595539d885a42ee9ac562f4f0786",
+    "url": "/%7Bex-pay%7D/static/media/login_img2.96165955.png"
+  },
+  {
+    "revision": "cd0682a7288ec5946dbbfa957fa7348d",
+    "url": "/%7Bex-pay%7D/static/media/mac.cd0682a7.png"
+  },
+  {
+    "revision": "062b76b917f29fd53a7e9a7457c6254c",
+    "url": "/%7Bex-pay%7D/static/media/map.062b76b9.png"
+  },
+  {
+    "revision": "bc984ba92c5535beb7bb6993f3786bca",
+    "url": "/%7Bex-pay%7D/static/media/map.bc984ba9.png"
+  },
+  {
+    "revision": "9c802a0539d2dff83aa6de8f436a00bd",
+    "url": "/%7Bex-pay%7D/static/media/new_shape.9c802a05.png"
+  },
+  {
+    "revision": "2751be0438d46b2f3144c1be9117b20b",
+    "url": "/%7Bex-pay%7D/static/media/post_img_1.2751be04.png"
+  },
+  {
+    "revision": "1a465e21b191cd85861778c6d8475d1c",
+    "url": "/%7Bex-pay%7D/static/media/post_img_2.1a465e21.png"
+  },
+  {
+    "revision": "fb60556959309df6cf78503816f67f74",
+    "url": "/%7Bex-pay%7D/static/media/post_img_3.fb605569.png"
+  },
+  {
+    "revision": "fb692b2f0595b9e357336e13a54be439",
+    "url": "/%7Bex-pay%7D/static/media/price_bg.fb692b2f.png"
+  },
+  {
+    "revision": "ebb79ad7930a3e3ba83cf7d5b0933c5f",
+    "url": "/%7Bex-pay%7D/static/media/shape-footer.ebb79ad7.png"
+  },
+  {
+    "revision": "49d71d788158faa35d0c5d65f17c808b",
+    "url": "/%7Bex-pay%7D/static/media/shape_bg.49d71d78.png"
+  },
+  {
+    "revision": "67f15a515cb5175dd7d117c72659b25d",
+    "url": "/%7Bex-pay%7D/static/media/shape_two.67f15a51.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/%7Bex-pay%7D/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/%7Bex-pay%7D/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/%7Bex-pay%7D/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/%7Bex-pay%7D/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c3efe460034305e25444f512984c06c1",
+    "url": "/%7Bex-pay%7D/static/media/solution_01.c3efe460.jpg"
+  },
+  {
+    "revision": "3bba8baf38766f61299723667645f345",
+    "url": "/%7Bex-pay%7D/static/media/solution_02.3bba8baf.jpg"
+  },
+  {
+    "revision": "f99f361bd58e85f3dc2cdceaeea4da44",
+    "url": "/%7Bex-pay%7D/static/media/startup_banner_bg.f99f361b.png"
+  },
+  {
+    "revision": "b143a12c46490825b7e0f05d5a8af715",
+    "url": "/%7Bex-pay%7D/static/media/startup_banner_img.b143a12c.png"
+  },
+  {
+    "revision": "ad96aba577f48bfe86b3e80256395903",
+    "url": "/%7Bex-pay%7D/static/media/startup_shap.ad96aba5.png"
+  },
+  {
+    "revision": "a989a4a0def7def8719104ff70a11dfe",
+    "url": "/%7Bex-pay%7D/static/media/svg.a989a4a0.svg"
+  },
+  {
+    "revision": "f0823274cf3b883d70592402ed4bfba0",
+    "url": "/%7Bex-pay%7D/static/media/tab_shape_bg.f0823274.png"
+  },
+  {
+    "revision": "3e65fbfbee4c78d90845f30f4f306228",
+    "url": "/%7Bex-pay%7D/static/media/testimonial_bg.3e65fbfb.png"
+  },
+  {
+    "revision": "87d0684dab92a92eb702810077756814",
+    "url": "/%7Bex-pay%7D/static/media/testimonial_bg_shap.87d0684d.png"
+  },
+  {
+    "revision": "508d6210a3eff3050dffec0fd69c4357",
+    "url": "/%7Bex-pay%7D/static/media/testimonial_bg_two.508d6210.png"
+  },
+  {
+    "revision": "99b2de0a56a94578defbdea0ce7915f6",
+    "url": "/%7Bex-pay%7D/static/media/testimonial_img.99b2de0a.png"
+  },
+  {
+    "revision": "44fd972fcbfadb458be13b13029cc34c",
+    "url": "/%7Bex-pay%7D/static/media/testimonial_img2.44fd972f.png"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/%7Bex-pay%7D/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/%7Bex-pay%7D/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/%7Bex-pay%7D/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/%7Bex-pay%7D/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "1aaf50b99896a5952606ca0cdb6faf06",
+    "url": "/%7Bex-pay%7D/static/media/video_bg.1aaf50b9.jpg"
+  },
+  {
+    "revision": "33e069120a1c4e7b3f416a93477c7aa0",
+    "url": "/%7Bex-pay%7D/static/media/video_img_01.33e06912.png"
+  },
+  {
+    "revision": "d909bfe08f4c120b557a9edc44e14b46",
+    "url": "/%7Bex-pay%7D/static/media/video_img_02.d909bfe0.jpg"
+  },
+  {
+    "revision": "4686b77ff21a8d2a16096734451aff38",
+    "url": "/%7Bex-pay%7D/static/media/web_image.4686b77f.jpg"
+  }
+]);
